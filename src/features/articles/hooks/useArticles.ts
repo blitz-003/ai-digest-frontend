cd8b9984c97e6d85743b5fbff27f9be9c4a2e9ec
blitz-003 <elmanpathan@gmail.com>
2026-07-18 02:40:53 +0600

    added log

--- src/features/articles/hooks/useArticles.ts
+++ src/features/articles/hooks/useArticles.ts
@@ -5,7 +5,9 @@ import { getArticles } from "../api/article.api";
 export function useArticles(params?: any) {
   return useQuery({
     queryKey: ["articles", params],
-
-    queryFn: () => getArticles(params),
+    queryFn: async () => {
+      console.log("FETCHING ARTICLES");
+      return getArticles(params);
+    },
   });
 }
