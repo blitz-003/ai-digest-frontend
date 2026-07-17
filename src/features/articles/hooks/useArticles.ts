4ea37567030e7a6d9b97669b1d2046b8b4cdf7c5
blitz-003 <elmanpathan@gmail.com>
2026-07-18 03:08:50 +0600

    changed the remount parameter

--- src/features/articles/hooks/useArticles.ts
+++ src/features/articles/hooks/useArticles.ts
@@ -5,9 +5,8 @@ import { getArticles } from "../api/article.api";
 export function useArticles(params?: any) {
   return useQuery({
     queryKey: ["articles", params],
-    queryFn: async () => {
-      console.log("FETCHING ARTICLES");
-      return getArticles(params);
-    },
+    queryFn: () => getArticles(params),
+    staleTime: 0,
+    refetchOnMount: true,
   });
 }
